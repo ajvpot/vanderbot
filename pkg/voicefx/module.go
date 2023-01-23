9b441b5b774aa3e34b1ab7092117c8b0e2fc1bf8
Alex Vanderpot <alex@vanderpot.com>
2023-01-23 15:59:13 -0800

    ci

--- pkg/voicefx/module.go
+++ pkg/voicefx/module.go
@@ -19,10 +19,18 @@ type Result struct {
 }
 
 func Register(p Params) {
+	joined := false
 	p.Session.AddHandler(func(s *discordgo.Session, m *discordgo.VoiceServerUpdate) {
 		p.Log.Debug("VoiceServerUpdate", zap.Reflect("event", m))
 	})
 	p.Session.AddHandler(func(s *discordgo.Session, m *discordgo.VoiceStateUpdate) {
 		p.Log.Debug("VoiceStateUpdate", zap.Reflect("event", m))
+		if !joined {
+			cv, _ := s.ChannelVoiceJoin(m.GuildID, m.ChannelID, true, false)
+			cv.AddHandler(func(vc *discordgo.VoiceConnection, vs *discordgo.VoiceSpeakingUpdate) {
+				p.Log.Debug("voiceSpeakingUpdate", zap.Reflect("conn", vc), zap.Reflect("speaking", vs))
+			})
+			joined = true
+		}
 	})
 }
