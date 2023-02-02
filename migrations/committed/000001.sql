af870c4bfafe2659d839ddad03911168d3e1d7e9
Alex Vanderpot <alex@vanderpot.com>
2023-02-01 16:17:44 -0800

    migration

--- migrations/committed/000001.sql
+++ migrations/committed/000001.sql
@@ -1,12 +1,12 @@
 --! Previous: -
---! Hash: sha1:d7dc968634be4da40d717fddcef4b62031e6ceb3
+--! Hash: sha1:61c2131138e47edbed3fc17a7d89e030420f73cf
 
 -- Enter migration here
 DROP TABLE IF EXISTS presence CASCADE;
 
 CREATE TABLE presence
 (
-    user_id  BIGINT,
-    guild_id BIGINT,
-    blob     jsonb
+    user_id  BIGINT NOT NULL,
+    guild_id BIGINT NOT NULL,
+    blob     jsonb  NOT NULL
 );
